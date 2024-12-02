3baaefc1b214636500a88a439bc06789941e670b
s1nbo <77055060+s1nbo@users.noreply.github.com>
2024-12-02 14:26:36 +0100

    Update and rename 1_24.py to 01_24.py

--- 2024/01_24.py
+++ 2024/01_24.py
@@ -3,7 +3,7 @@ ans1 = 0
 ans2 = 0
 
 # Read input
-for line in open('1_12_24_data.csv'):
+for line in open('01_24_data.csv'):
     line = line.split('   ')
     left.append(int(line[0]))
     right.append(int(line[1]))
