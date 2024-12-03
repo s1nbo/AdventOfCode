a0aef98f75864944592157301a64ff3ef004fd93
s1nbo <evilbeancmd@gmail.com>
2024-12-03 23:08:01 +0100

    fixed question 1

--- 2024/01.py
+++ 2024/01.py
@@ -3,7 +3,7 @@ ans1 = 0
 ans2 = 0
 
 # Read input
-for line in open('01.csv'):
+for line in open('01_data.txt'):
     line = line.split('   ')
     left.append(int(line[0]))
     right.append(int(line[1]))
